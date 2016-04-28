1d8de6963cf62e3bfc9dfc2ae969b62d72f111de
amor <amor_zhang@icloud.com>
2016-04-28 16:12:04 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @Description 系统配置
+ * @Author Amor
+ * @Created 2016/04/26 11:48
+ * 技术只是解决问题的选择,而不是解决问题的根本...
+ * 我是Amor,为发骚而生!
+ *
+ */
+
+module.exports = {
+    upload: {
+        path: process.cwd() + '/uploads'
+    }
+}
