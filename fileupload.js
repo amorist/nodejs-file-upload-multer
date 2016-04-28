c4a3e752a60daee7480c35a7935f9800ea3e768c
amor <amor_zhang@icloud.com>
2016-04-28 16:40:52 +0800

    Update fileupload.js

--- fileupload.js
+++ fileupload.js
@@ -13,6 +13,7 @@ var config = require('./config')
 
 var storage = multer.diskStorage({
     //设置上传文件路径,以后可以扩展成上传至七牛,文件服务器等等
+    //Note:如果你传递的是一个函数，你负责创建文件夹，如果你传递的是一个字符串，multer会自动创建
     destination: config.upload.path,
     //TODO:文件区分目录存放
     //给上传文件重命名，获取添加后缀名,获取文件MD5,文件重复直接覆盖
